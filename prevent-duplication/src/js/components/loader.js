a1476832c2f1b1dcc6b206a40aa54ffddbfc307a
nestorbeltran <nestor.beltran@gmail.com>
2020-03-04 12:41:37 -0600

    class 18

---
+++ prevent-duplication/src/js/components/loader.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Loader = ({data}) =>{
+  return(
+    <li>{data.name}</li>
+  )
+}
+
+export default Loader;
---
+++ prevent-duplication/src/js/text.js
@@ -0,0 +1,6 @@
+
+const text = () => {
+    document.body.innerHTML = '<p>Hi Home !</p>';
+}
+
+export default text;
